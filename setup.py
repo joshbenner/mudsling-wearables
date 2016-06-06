40533a4e067ca1fbf94aba4d84c66a2df27f4d9c
Josh Benner <josh@bennerweb.com>
2016-06-05 22:19:43 -0400

    Initial import.

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='mudsling-wearables',
+    license='MIT',
+    author='Josh Benner',
+    author_email='josh@bennerweb.com',
+
+    packages=find_packages(),
+    include_package_data=True,
+    use_scm_version=True,
+    setup_requires=['setuptools_scm'],
+    install_requires=[
+        'mudsling',
+    ],
+
+    entry_points={
+        'mudsling.plugin': [
+            'wearables = wearables:WearablesPlugin'
+        ]
+    }
+)
